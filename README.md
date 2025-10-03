ebfd9cde208d60dd1e9ea3613b2cc2647deb309f
WarrenWu4 <warrenweiwu04@gmail.com>
2025-10-03 13:44:40 -0500

    peter pan

---
+++ README.md
@@ -0,0 +1,16 @@
+# wallflower
+
+GUI interface for hyprpaper.
+
+## Installation
+
+```bash
+```
+
+## Usage
+
+- by default will find images from ~/background
+- can add and change multiple directories for finding images
+- runs a script through hyprpaper to change the background
+
+## Configuration 
---
+++ src/Main.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLabels #-}
+
+module Main (main) where
+
+import Control.Monad (void)
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
+
+main :: IO ()
+main = void $ do
+  app <- new Gtk.Application [ #applicationId := "com.wallflower.start "]
+  - <- on app #activate $ do
+    window <- new Gtk.ApplicationWindow [ #application := app
+                                        , #title := "Wallflower"
+                                        , #defaultWidth := 800
+                                        , #defaultHeight := 600
+                                        ]
+    label <- new Gtk.Label [ #label := "Testing" ]
+    #setChild window (Just label)
+    #present window
+    Gtk.applicationRun app Nothing
