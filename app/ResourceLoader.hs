87b68f7e9f8b4746490658755247d3a84c69817c
WarrenWu4 <warrenweiwu04@gmail.com>
2025-10-05 01:07:58 -0500

    feat: changed vbox to grid + cleaned up some code in load resources

--- app/ResourceLoader.hs
+++ app/ResourceLoader.hs
@@ -9,30 +9,36 @@ module ResourceLoader
   )
 where
 
-import Data.GI.Base
+import Control.Monad (forM_)
+import Data.Int
+import Data.Text.Internal
 import qualified GI.Gdk as Gdk
-import qualified GI.Gio as Gio
 import qualified GI.Gtk as Gtk
 import MarkupInjector
 import Paths_wallflower (getDataFileName)
-import System.IO (readFile)
-import Data.Text.Internal
 
 loadResource :: FilePath -> IO String
 loadResource path = do
   absPath <- getDataFileName path
-  contents <- readFile absPath
-  return contents
+  readFile absPath
 
 getResourcePath :: FilePath -> IO FilePath
 getResourcePath path = do
-  absPath <- getDataFileName path
-  return absPath
+  getDataFileName path
 
 getImageById :: Gtk.Builder -> Text -> IO Gtk.Picture
 getImageById builder objectId = do
-  Just obj <- Gtk.builderGetObject builder objectId 
+  Just obj <- Gtk.builderGetObject builder objectId
   Gtk.unsafeCastTo Gtk.Picture obj
+
+placeImages :: Gtk.Grid -> [Gtk.Picture] -> IO ()
+placeImages container imgs = do
+  let indexedImgs = zip [0..(length imgs - 1)] imgs
+  forM_ indexedImgs $ \(i, img) -> do
+    let num = fromIntegral i :: Int32
+    let col = num `mod` 3 :: Int32
+    let row = num `div` 3 :: Int32
+    Gtk.gridAttach container img col row 1 1
 
 loadUI :: Gtk.Application -> [String] -> IO ()
 loadUI app imageMarkups = do
@@ -47,13 +53,13 @@ loadUI app imageMarkups = do
   Just winObj <- Gtk.builderGetObject builder "main_window"
   window <- Gtk.unsafeCastTo Gtk.ApplicationWindow winObj
 
-  let imageIds = ["background-image-"++ show n | n <- [1..length imageMarkups]]
+  let imageIds = ["background-image-" ++ show n | n <- [1 .. length imageMarkups]]
   imgs <- mapM (getImageById builder . pack) imageIds
 
   Just imgContainerObj <- Gtk.builderGetObject builder "backgrounds"
-  imgContainer <- Gtk.unsafeCastTo Gtk.Box imgContainerObj
+  imgContainer <- Gtk.unsafeCastTo Gtk.Grid imgContainerObj
 
-  mapM_ (Gtk.boxAppend imgContainer) imgs
+  placeImages imgContainer imgs
 
   #setApplication window (Just app)
 
