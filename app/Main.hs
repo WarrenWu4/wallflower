88594387b03a019551bff7dcc982bdbc42ad737c
WarrenWu4 <warrenweiwu04@gmail.com>
2025-10-05 22:04:18 -0500

    feat: added basic depedency validation

--- app/Main.hs
+++ app/Main.hs
@@ -7,20 +7,24 @@ import Control.Monad (void, zipWithM)
 import Data.GI.Base
 import DirectoryManager
 import qualified GI.Gtk as Gtk
-import ResourceLoader
 import MarkupInjector
+import ResourceLoader
+import Validator (checkHyprland, checkHyprpaper)
 
 main :: IO ()
 main = void $ do
+  checkHyprland
+  checkHyprpaper
+
   app <- new Gtk.Application [#applicationId := "com.warrenwu.wallflower"]
 
   let searchDirectories = getDirectoriesFromSetting ""
   imagePaths <- getImagesInDirectories searchDirectories
-  imageMarkups <- zipWithM createImageMarkup imagePaths [1..(length imagePaths)]
-  
+  imageMarkups <- zipWithM createImageMarkup imagePaths [1 .. (length imagePaths)]
+
   _ <- on app #activate $ do
     loadCSS
-    loadUI app imageMarkups 
+    loadUI app imageMarkups
 
   _ <- #run app Nothing
   return ()
--- app/Validator.hs
+++ app/Validator.hs
@@ -1,23 +1,36 @@
-module Validator (
-  checkHyprland,
-  checkHyprpaper
-) where
+module Validator
+  ( checkHyprland,
+    checkHyprpaper,
+  )
+where
+
+import Control.Monad (unless)
+import System.Directory (doesFileExist, getHomeDirectory, findExecutable)
+import Data.Maybe (isJust)
 
 -- all program depedency checks
 
--- TODO: implement
--- | check that hyprland is the wm
--- if not crash & send error to user to switch 
+-- | ensures that hyprland is installed and configured
+-- if not crash & send error to user to switch
 checkHyprland :: IO ()
-checkHyprland = do return ()
+checkHyprland = do
+  hyprPath <- findExecutable "hyprland"
+  unless (isJust hyprPath) $ error "Hyprland not found in PATH"
+  homeDir <- getHomeDirectory
+  let temp = homeDir <> "/.config/hypr/hyprland.conf"
+  configExists <- doesFileExist temp 
+  unless configExists $ error "Hyprland config not found"
+  print "Hyprland check passed"
 
--- TODO: implement
 -- | checks that hyprpaper is installed and configured
 -- if not crash & send error to user to setup
 checkHyprpaper :: IO ()
-checkHyprpaper = do return ()
-
-
-
-
+checkHyprpaper = do
+  hyprPaperPath <- findExecutable "hyprpaper"
+  unless (isJust hyprPaperPath) $ error "Hyprpaper not found in PATH"
+  homeDir <- getHomeDirectory
+  let temp = homeDir <> "/.config/hypr/hyprpaper.conf"
+  configExists <- doesFileExist temp 
+  unless configExists $ error "Hyprpaper config not found"
+  print "Hyprpaper check passed"
 
