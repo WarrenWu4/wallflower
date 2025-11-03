b69860b8e53e8ffd9675f16982b7d84c5dc2b1bc
WarrenWu4 <warrenweiwu04@gmail.com>
2025-11-03 16:39:47 -0600

    feat: added makeshift settings features

--- app/Settings.hs
+++ app/Settings.hs
@@ -5,7 +5,10 @@
 module Settings where
 
 import Control.Lens
+import LoggerGe
 import Monomer
+import Graphics.UI.TinyFileDialogs (selectFolderDialog)
+import Data.Text (pack)
 
 data SettingsModel = SettingsModel
   { _searchDirectories :: [String]
@@ -14,6 +17,9 @@ data SettingsModel = SettingsModel
 
 data SettingsEvent
   = SettingsInit
+  | SettingsBrowseFolders
+  | SettingsAddFolder String
+  | SettingsNone
   deriving (Eq, Show)
 
 type SettingsEnv = WidgetEnv SettingsModel SettingsEvent
@@ -28,13 +34,37 @@ defaultSettingsModel =
     { _searchDirectories = []
     }
 
+browseFoldersHandler :: IO SettingsEvent
+browseFoldersHandler = do
+  logMsg DEBUG "Browsing folders..."
+  folder <- selectFolderDialog "Select Wallpaper Folders" "/home/warrenwu/backgrounds"
+  case folder of
+    Just f -> logMsg DEBUG "Folder selected"
+    Nothing -> logMsg DEBUG "No folder selected"
+  return SettingsNone
+
 buildUISettings :: SettingsEnv -> SettingsModel -> SettingsNode
 buildUISettings wenv model = widgetTree
   where
-    widgetTree = vstack [label "Settings"]
+    widgetTree = 
+      vscroll 
+        ( vstack_
+            [childSpacing_ 12]
+            [ 
+              box_ [onClick SettingsBrowseFolders] (label "folder"),
+              vstack_
+                [childSpacing_ 4]
+                directories
+            ]
+        )
+
+    directories :: [SettingsNode] = map (\directory -> label (pack directory)) (model ^. searchDirectories)
 
 handleEventSettings :: SettingsEnv -> SettingsNode -> SettingsModel -> SettingsEvent -> [EventResponse SettingsModel SettingsEvent sp ep]
 handleEventSettings wenv node model evt = case evt of
   SettingsInit -> [] 
+  SettingsBrowseFolders -> [Task browseFoldersHandler]
+  SettingsAddFolder path -> [Model $ model & searchDirectories .~ snoc (model ^. searchDirectories) path]
+  SettingsNone -> []
 
 
