d381ccd1d871d0708b1d39e50aa409f42aa89ce0
WarrenWu4 <warrenweiwu04@gmail.com>
2025-11-17 00:11:27 -0600

    fix: scroll now smoother + less slow

--- app/Wallpapers.hs
+++ app/Wallpapers.hs
@@ -5,19 +5,22 @@
 module Wallpapers where
 
 import Control.Lens
+import Control.Monad (zipWithM)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.Maybe (catMaybes)
 import Data.Text (pack)
+import HyprpaperManager (applyWallpaper)
+import ImageDimension (getImageDimension, getImageFormat)
+import LoggerGe
 import Monomer
 import qualified Monomer.Lens as L
-import LoggerGe
-import HyprpaperManager (applyWallpaper)
 import UiData (getWallpaperData)
-import ImageDimension (getImageFormat, getImageDimension)
-import Control.Monad (zipWithM)
-import Data.Maybe (catMaybes)
 
 data WallpaperModel = WallpaperModel
   { _wallpaperPaths :: [String],
-    _wallpaperAspectRatios :: [Double]
+    _wallpaperAspectRatios :: [Double],
+    _wallpaperImages :: ByteString
   }
   deriving (Eq, Show)
 
@@ -25,6 +28,7 @@ data WallpaperEvent
   = WallpaperInit
   | LoadWallpapers [String]
   | LoadWallpaperDimensions [Double]
+  | LoadWallpaperImages ByteString
   | SetWallpaper String
   | WallpaperNone
   deriving (Eq, Show)
@@ -39,14 +43,26 @@ defaultWallpaperModel :: WallpaperModel
 defaultWallpaperModel =
   WallpaperModel
     { _wallpaperPaths = [],
-      _wallpaperAspectRatios = []
+      _wallpaperAspectRatios = [],
+      _wallpaperImages = ""
     }
 
 wallpaperInit :: IO WallpaperEvent
-wallpaperInit = do
-  logMsg DEBUG "Wallpaper Init Ran"
-  wallpaperData <- getWallpaperData
-  return (LoadWallpapers $ map (\(_, _, path) -> path) wallpaperData)
+wallpaperInit =
+  do
+    LoadWallpapers . map (\(_, _, path) -> path)
+    <$> getWallpaperData
+
+wallpaperImageInit :: IO WallpaperEvent
+wallpaperImageInit = do
+  -- wallpaperData <- getWallpaperData
+  -- let filePaths = map (\(_, _, path) -> path) wallpaperData
+  let filePath = "/home/warrenwu/screenshots/2025-11-16_23-34.png"
+  imageData <- BS.readFile filePath
+  return (LoadWallpaperImages imageData)
+
+-- imageData <- mapM BS.readFile filePaths
+-- return (LoadWallpaperImages imageData)
 
 wallpaperDimInit :: IO WallpaperEvent
 wallpaperDimInit = do
@@ -59,7 +75,6 @@ wallpaperDimInit = do
   let aspectRatios = [(\num den -> fromIntegral num / fromIntegral den) h w | (w, h) <- validResults]
   return $ LoadWallpaperDimensions aspectRatios
 
-
 -- TODO: figure some better way to do this dynamic sizing crap
 getWallpaperColumnSize :: Double -> Double
 getWallpaperColumnSize windowWidth = do
@@ -69,11 +84,11 @@ getWallpaperColumnSize windowWidth = do
   let numColumns = 3
   (windowWidth - windowPadding * 2 - columnGap * (numColumns - 1)) / numColumns
 
-setWallpaperHandler :: String -> IO WallpaperEvent 
+setWallpaperHandler :: String -> IO WallpaperEvent
 setWallpaperHandler path = do
   logMsg DEBUG $ "Setting wallpaper to: " ++ path
   applyWallpaper path
-  return WallpaperNone 
+  return WallpaperNone
 
 buildUIWallpaper :: WallpaperEnv -> WallpaperModel -> WallpaperNode
 buildUIWallpaper wenv model = widgetTree
@@ -82,7 +97,8 @@ buildUIWallpaper wenv model = widgetTree
     columnWidth = getWallpaperColumnSize windowWidth
 
     widgetTree =
-      vscroll
+      vscroll_
+        [wheelRate 80]
         ( hstack_
             [childSpacing_ 12]
             [ vstack_ [childSpacing_ 12] col1 `styleBasic` [],
@@ -91,18 +107,26 @@ buildUIWallpaper wenv model = widgetTree
             ]
         )
 
-    imageButton :: String -> Double -> WallpaperNode
-    imageButton p ar = box_ [onClick (SetWallpaper p)] (image_ (pack p) [fitWidth] `styleBasic` [width columnWidth, height (ar * columnWidth)])
+    imageButton :: (String, Double) -> WallpaperNode
+    imageButton (p, ar) =
+      box_
+        [onClick (SetWallpaper p)]
+        ( image_ (pack p) [fitWidth] `styleBasic` [width columnWidth, height $ columnWidth * ar]
+        )
 
-    indexedList = zip3 [0..] (model ^. wallpaperPaths) (model ^. wallpaperAspectRatios)
-    col1 :: [WallpaperNode] = map (uncurry imageButton) $ [(p, ar) | (i, p, ar) <- indexedList, i `mod` 3 == 0] 
-    col2 :: [WallpaperNode] = map (uncurry imageButton) $ [(p, ar) | (i, p, ar) <- indexedList, i `mod` 3 == 1] 
-    col3 :: [WallpaperNode] = map (uncurry imageButton) $ [(p, ar) | (i, p, ar) <- indexedList, i `mod` 3 == 2] 
+    -- static image issue caused by wrong encoding format in bytestring
+    -- testImage :: WallpaperNode = imageMem_ "testing" (model ^. wallpaperImages) Size{_sW=55, _sH=47} []
+
+    indexedList = zip3 [0 ..] (model ^. wallpaperPaths) (model ^. wallpaperAspectRatios)
+    col1 :: [WallpaperNode] = map imageButton $ [(p, ar) | (i, p, ar) <- indexedList, i `mod` 3 == 0]
+    col2 :: [WallpaperNode] = map imageButton $ [(p, ar) | (i, p, ar) <- indexedList, i `mod` 3 == 1]
+    col3 :: [WallpaperNode] = map imageButton $ [(p, ar) | (i, p, ar) <- indexedList, i `mod` 3 == 2]
 
 handleEventWallpaper :: WallpaperEnv -> WallpaperNode -> WallpaperModel -> WallpaperEvent -> [EventResponse WallpaperModel WallpaperEvent sp ep]
 handleEventWallpaper wenv node model evt = case evt of
-  WallpaperInit -> [Task wallpaperInit, Task wallpaperDimInit]
+  WallpaperInit -> [Task wallpaperInit, Task wallpaperImageInit, Task wallpaperDimInit]
   LoadWallpapers paths -> [Model $ model & wallpaperPaths .~ paths]
   LoadWallpaperDimensions dims -> [Model $ model & wallpaperAspectRatios .~ dims]
+  LoadWallpaperImages images -> [Model $ model & wallpaperImages .~ images]
   SetWallpaper path -> [Task $ setWallpaperHandler path]
   WallpaperNone -> []
