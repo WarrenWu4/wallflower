74ada6f91e14e00ce50bce2faec53f5ba974df07
WarrenWu4 <warrenweiwu04@gmail.com>
2025-12-03 11:58:03 -0600

    feat: added hyprpaper config parser in cpp

--- external/image_utils.cpp
+++ external/image_utils.cpp
@@ -1,9 +1,43 @@
-bool image_test_cpp() {
-  return true;
-}
+#include <cstdint>
+#include <memory>
+
+struct Size {
+  int width;
+  int height;
+};
+
+struct Pixel {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  uint8_t a;
+};
+
+class Png {
+public:
+  Size getImageDimension(const char *filePath) { return {0, 0}; }
+  Pixel* getImagePixels(const char *filePath) {
+    std::unique_ptr<Pixel> pixels = std::make_unique<Pixel>();
+    return pixels.release();
+  }
+};
+
+class Jpg {
+public:
+  Size getImageDimension(const char *filePath) { return {0, 0}; }
+  Pixel* getImagePixels(const char *filePath) {
+    std::unique_ptr<Pixel> pixels = std::make_unique<Pixel>();
+    return pixels.release();
+  }
+};
 
 extern "C" {
-    bool image_test() {
-        return image_test_cpp();
-    }
+Size get_png_dimension(const char *filePath) {
+  Png png;
+  return png.getImageDimension(filePath);
+}
+Size get_jpg_dimension(const char *filePath) {
+  Jpg jpg;
+  return jpg.getImageDimension(filePath);
+}
 }
