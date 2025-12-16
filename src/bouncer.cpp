41730955e37dab03ecd1d4b0174511f2925e5db7
WarrenWu4 <warrenweiwu04@gmail.com>
2025-12-16 08:13:08 -0600

    feat: added skeleton for hypr management and validation checks

---
+++ src/bouncer.cpp
@@ -0,0 +1,22 @@
+// perform these checks to ensure program can run smoothly
+// just like a bouncer at the club O-O
+
+bool doesHyprlandExist() {
+  return false;
+}
+
+bool isHyprlandRunning() {
+  return false;
+}
+
+bool doesHyprpaperExist() {
+  return false;
+}
+
+bool isHyprpaperRunning() {
+  return false;
+}
+
+bool isHyprpaperIpcEnabled() {
+  return false;
+}
