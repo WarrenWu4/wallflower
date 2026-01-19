3cc1cf7bb2f5403d14443a9fbc83ed92f4c58098
WarrenWu4 <warrenweiwu04@gmail.com>
2026-01-19 14:22:53 -0600

    feat: added function to get active monitors and api exposed from configuration class

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -4,6 +4,9 @@
 #include <cassert>
 #include <filesystem>
 #include <fstream>
+#include <X11/Xlib.h>
+#include <X11/extensions/Xrandr.h>
+#include <wayland-client.h>
 
 WallflowerConfig Configuration::getDefaultConfig() {
   return (WallflowerConfig){
@@ -15,6 +18,54 @@ WallflowerConfig Configuration::getDefaultConfig() {
       .preferences = {},
       .searchPaths = {},
   };
+}
+
+std::vector<MonitorInfo> Configuration::getActiveMonitors() {
+  std::vector<MonitorInfo> monitors = {};
+  
+  // try to open display
+  Display* display = XOpenDisplay(nullptr);
+  if (!display) {
+    Logger::logMsg(LogLabel::FAIL, "Could not open display");
+    return monitors;
+  }
+  
+  Window root = DefaultRootWindow(display);
+  
+  // get screen resources using XRandR
+  XRRScreenResources* resources = XRRGetScreenResources(display, root);
+  if (!resources) {
+    Logger::logMsg(LogLabel::FAIL, "Could not get resources using XRandR");
+    XCloseDisplay(display);
+    return monitors;
+  }
+  
+  // iterate through all outputs
+  for (int i = 0; i < resources->noutput; i++) {
+    XRROutputInfo* outputInfo = XRRGetOutputInfo(display, resources, resources->outputs[i]);
+    
+    if (outputInfo->connection == RR_Connected && outputInfo->crtc) {
+      // get monitor information through XRRGetCrtcInfo
+      XRRCrtcInfo* crtcInfo = XRRGetCrtcInfo(display, resources, outputInfo->crtc); 
+      MonitorInfo monitor;
+      monitor.name = outputInfo->name;
+      monitor.x = crtcInfo->x;
+      monitor.y = crtcInfo->y;
+      monitor.width = crtcInfo->width;
+      monitor.height = crtcInfo->height;
+      
+      // Check if this is the primary monitor
+      RROutput primary = XRRGetOutputPrimary(display, root);
+      monitor.primary = (resources->outputs[i] == primary);
+      
+      monitors.push_back(monitor);
+      XRRFreeCrtcInfo(crtcInfo);
+    }
+    XRRFreeOutputInfo(outputInfo);
+  }
+  XRRFreeScreenResources(resources);
+  XCloseDisplay(display);
+  return monitors;
 }
 
 void Configuration::printWallflowerConfig() {
@@ -41,8 +92,16 @@ void Configuration::printWallflowerConfig() {
   Logger::logMsg(LogLabel::DEBUG, "Wallflower Config\n" + msg);
 }
 
+void Configuration::printMonitorInformation() {
+  for (const MonitorInfo& monitor : monitors) {
+    std::string msg = "Monitor found: " + monitor.name + ", {x: " + std::to_string(monitor.x) + ", y: " + std::to_string(monitor.y) + "}, {width: " + std::to_string(monitor.width) + ", height: " + std::to_string(monitor.height) + "}" + ", " + (monitor.primary ? "primary" : "not primary");
+    Logger::logMsg(LogLabel::DEBUG, msg);
+  }
+}
+
 Configuration::Configuration() {
   config = getDefaultConfig();
+  monitors = getActiveMonitors();
   hyprpaperConfigPath = std::filesystem::path(std::string(getenv("HOME")) +
                                               "/.config/hypr/hyprpaper.conf");
   saveFilePath = Utils::getSaveFilePath();
@@ -71,6 +130,10 @@ Configuration::~Configuration() {
   writeHyprpaperConf();
   writeWallflowerSave();
 }
+
+const std::vector<MonitorInfo>& Configuration::getMonitors() {
+  return monitors;
+} 
 
 void Configuration::updateWallpaper(std::string display, std::string path,
                                     FitMode mode) {
