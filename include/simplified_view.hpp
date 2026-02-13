9513894257a007123fd3e7bb23f6360981b10e36
WarrenWu4 <warrenweiwu04@gmail.com>
2026-02-12 23:14:03 -0600

    feat: added skeleton for simplified view

---
+++ include/simplified_view.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class SimplifiedView {
+private:
+  void WallpaperSelectorEl();
+  void WallpaperDisplayEl();
+public:
+  SimplifiedView();
+  ~SimplifiedView();
+
+  void SimplifiedViewEl();
+};
